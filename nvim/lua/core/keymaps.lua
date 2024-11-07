24e7abe774c60de07f7456a9eb87ca3891224cb4
Sriram Sundarraj <sriram@retool.com>
2024-11-06 16:55:22 -0800

    nvim

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,47 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- save with ctrl s
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- Stay in indent mode when indenting
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,23 @@
+vim.opt.number = true -- Make line numbers default (default: false)
+vim.opt.relativenumber = true -- Set relative numbered lines (default: false)
+
+-- vim.opt.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim. (default: '')
+
+vim.opt.wrap = false -- Display lines as one long line (default: true)
+vim.opt.linebreak = true -- Companion to wrap, don't split words (default: false)
+
+vim.opt.mouse = "a" -- Enable mouse mode (default: '')
+
+vim.opt.autoindent = true -- Copy indent from current line when starting new one (default: true)
+
+vim.opt.ignorecase = true -- Case-insensitive searching UNLESS \C or capital in search (default: false)
+vim.opt.smartcase = true -- Smart case (default: false)
+
+vim.o.tabstop = 2 -- Insert n spaces for a tab (default: 8)
+vim.o.softtabstop = 2 -- Number of spaces that a tab counts for while performing editing operations (default: 0)
+vim.opt.expandtab = true -- Convert tabs to spaces (default: false)
+
+vim.o.splitbelow = true -- Force all horizontal splits to go below current window (default: false)
+vim.o.splitright = true -- Force all vertical splits to go to the right of current window (default: false)
+
+vim.o.scrolloff = 4 -- Minimal number of screen lines to keep above and below the cursor (default: 0)
