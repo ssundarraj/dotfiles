b35c6875d3874e5d9ca3fff99926f091d39ea787
Sriram Sundarraj <ssundarraj@gmail.com>
2024-11-17 11:35:30 -0800

    maximize keybinding

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
 vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
 vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sm", "<C-w>|", opts) -- maximize vertical split window
 vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
 
 -- Navigate between splits
