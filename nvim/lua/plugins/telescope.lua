0ccd77ba1ad2635c0568fc7e75b8fb1cd04025d5
Sriram Sundarraj <sriram@retool.com>
2024-11-18 10:52:00 -0800

    changed files hint

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -128,7 +128,7 @@ return { -- Fuzzy Finder (files, lsp, etc)tele
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader>sGS", builtin.git_status, { desc = "[S]earch [G]it [S]tatus" })
-		vim.keymap.set("n", "<leader>scf", changed_files, { desc = "[S]earch [G]it [S]tatus" })
+		vim.keymap.set("n", "<leader>scf", changed_files, { desc = "[S]earch [C]hanged [F]iles" })
 		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
