5748afe7dd8c14727c6b2b688ffcb5aa292159f4
Sriram Sundarraj <sriram@retool.com>
2024-11-26 09:17:46 -0800

    move core/* out

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
-require("core.options")
-require("core.keymaps")
+require("options")
+require("keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
