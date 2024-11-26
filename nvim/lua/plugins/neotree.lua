b121b2b9d50e4ab408a855606789603f21c4a2d9
Sriram Sundarraj <sriram@retool.com>
2024-11-26 08:58:28 -0800

    tab to open/close neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,13 +8,13 @@ return {
 	},
 	cmd = "Neotree",
 	keys = {
-		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+		{ "<Tab>", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
 	},
 	opts = {
 		filesystem = {
 			window = {
 				mappings = {
-					["\\"] = "close_window",
+					["<Tab>"] = "close_window",
 				},
 			},
 		},
