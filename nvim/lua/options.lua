8f2366d69f28f60151ad248566cb5533b80dc801
Sriram Sundarraj <sriram@retool.com>
2024-12-07 09:14:35 +0530

    bashls and highlight on yank

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -39,3 +39,14 @@ vim.opt.matchpairs:append("<:>")
 
 -- show sign column
 vim.opt.signcolumn = "number"
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -175,8 +175,7 @@ return {
 			--
 			-- But for many setups, the LSP (`ts_ls`) will work just fine
 			ts_ls = {},
-			--
-
+			bashls = {},
 			lua_ls = {
 				-- cmd = {...},
 				-- filetypes = { ...},
