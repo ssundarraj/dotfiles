3aade142e988ebab2122a8c93eae1cc4d25a81b5
Sriram Sundarraj <sriram@retool.com>
2025-02-03 22:26:08 -0800

    vim-rhubarb

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,14 @@
 return {
-	"tpope/vim-fugitive",
-	config = function()
-		vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "[G]it [B]lame" })
-	end,
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "[G]it [B]lame" })
+		end,
+	},
+	{
+		"tpope/vim-rhubarb",
+		dependencies = { "tpope/vim-fugitive" },
+		lazy = false,
+		cmd = { "GBrowse" },
+	},
 }
