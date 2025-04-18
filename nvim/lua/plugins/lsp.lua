def0e890787a494a45726822dcbccb7d5f2f422b
Sriram Sundarraj <sriram@retool.com>
2025-04-18 11:55:07 -0700

    ts_ls opts

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -168,20 +168,14 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			-- clangd = {},
-			-- gopls = {},
-			-- pyright = {},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
 			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
 			ts_ls = {
+				-- See https://github.com/typescript-language-server/typescript-language-server/blob/master/docs/configuration.md
 				init_options = {
 					maxTsServerMemory = 8192,
-					nodePath = "node",
+					tsdk = "node_modules/typescript/lib",
+					enablePromptUseWorkspaceTsdk = true,
 				},
 			},
 			bashls = {},
