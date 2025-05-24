b3b72b41e6c233c01eb567b87fb273a782f8f177
Sriram Sundarraj <sriram@retool.com>
2025-05-24 12:05:47 +0100

    GitLinker

--- nvim/lua/plugins/git-plugins.lua
+++ nvim/lua/plugins/git-plugins.lua
@@ -6,4 +6,13 @@ return {
 			require("gitsigns").setup()
 		end,
 	},
+	{
+		"linrongbin16/gitlinker.nvim",
+		cmd = "GitLink",
+		opts = {},
+		keys = {
+			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+		},
+	},
 }
