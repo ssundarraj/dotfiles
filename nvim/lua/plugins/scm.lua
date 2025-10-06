70fb58448f7cefc45cc10dc913ddf3c260fd0118
Sriram Sundarraj <ssundarraj@gmail.com>
2025-10-06 18:42:37 +0900

    default_branch gitlinker

--- nvim/lua/plugins/scm.lua
+++ nvim/lua/plugins/scm.lua
@@ -16,8 +16,8 @@ return {
 		cmd = "GitLink",
 		opts = {},
 		keys = {
-			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+			{ "<leader>gy", "<cmd>GitLink default_branch<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink! default_branch<cr>", mode = { "n", "v" }, desc = "Open git link" },
 		},
 	},
 	{
