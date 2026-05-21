fa2c56f08be3862a3afc8439595889cf32b2a51a
Sriram Sundarraj <ssundarraj@gmail.com>
2026-05-20 18:24:57 -0700

    nvim: md editing tweaks

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -57,6 +57,8 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "markdown",
 	callback = function()
 		vim.opt_local.wrap = true
+		vim.opt_local.textwidth = 0
+		vim.opt_local.formatoptions:remove({ "t", "a" })
 	end,
 })
 
