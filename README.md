a5e87f81d1731aab289c430b0fefca61ad710c20
Sriram Sundarraj <ssundarraj@gmail.com>
2026-07-13 21:52:50 -0700

    nvim/ghostty: move smear cursor into ghostty config

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Symlink opencode TUI config
 
 `ln -sf ~/dev/dotfiles/opencode/tui.json ~/.config/opencode/tui.json`
 
+Symlink Ghostty config
+
+`mkdir -p ~/.config/ghostty`
+
+`ln -s ~/dev/dotfiles/ghostty ~/.config/ghostty`
+
 Global Git Ignore
 
 Located at `~/.config/git/ignore`. Currently just copied manually from `./.gitignore`.
--- nvim/init.lua
+++ nvim/init.lua
@@ -88,20 +88,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"sphamba/smear-cursor.nvim",
-		opts = { -- Default  Range
-			stiffness = 0.8, -- 0.6      [0, 1]
-			trailing_stiffness = 0.6, -- 0.45     [0, 1]
-			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			damping = 0.95, -- 0.85     [0, 1]
-			damping_insert_mode = 0.95, -- 0.9      [0, 1]
-			distance_stop_animating = 0.5, -- 0.1      > 0
-			min_horizontal_distance_smear = 70,
-			min_vertical_distance_smear = 3,
-		},
-	},
-	{
 		"tzachar/local-highlight.nvim",
 		config = function()
 			require("local-highlight").setup({
